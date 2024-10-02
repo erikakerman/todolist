cc1c30896fcd8f3f11ee032673c2d6281524d790
Erik Akerman <erikakerman@gmail.com>
2024-10-02 11:20:25 +0200

    Add project files.

---
+++ todolist/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Task> tasks = new List<Task>();
+
+        while (true)
+        {
+            Console.Write("Enter task title: ");
+            string title = Console.ReadLine();
+
+            Console.Write("Enter due date (yyyy-MM-dd): ");
+            DateTime dueDate = DateTime.Parse(Console.ReadLine());
+
+            tasks.Add(new Task(title, dueDate));
+
+            Console.Write("Add another task? (y/n): ");
+            if (Console.ReadLine().ToLower() != "y")
+                break;
+        }
+
+        Console.WriteLine("\nYour tasks:");
+        foreach (Task task in tasks)
+        {
+            Console.WriteLine(task);
+        }
+    }
+}
---
+++ todolist/Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Task
+{
+    public string Title { get; set; }
+    public DateTime DueDate { get; set; }
+
+    public Task(string title, DateTime dueDate)
+    {
+        Title = title;
+        DueDate = dueDate;
+    }
+
+    public override string ToString()
+    {
+        return Title + " - Due: " + DueDate.ToString("yyyy-MM-dd");
+    }
+}
